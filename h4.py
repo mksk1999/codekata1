6f26b5ff271b14bfa03e54f3bc55eafdebae9ab8
saikumar <51634270+mksk1999@users.noreply.github.com>
2019-07-16 13:26:34 +0530

    Create h4.py

---
+++ h4.py
@@ -0,0 +1,15 @@
+n=int(input())
+a=list(map(int,input().split()))
+count=dict()
+for word in a:
+    if word not in count:
+        count[word]=1
+    else:
+        count[word]+=1
+for word in a:
+    if (count[word]!=1):
+        print(word)
+        break
+    else:
+        print("unique")
+        break
