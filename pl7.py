b80ec6033df3b21ad3b081d01fe1f2ea530b01c5
saikumar <51634270+mksk1999@users.noreply.github.com>
2019-07-16 11:13:18 +0530

    Rename pl6.py to pl7.py

---
+++ pl7.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=list(map(int,input().split()))
+def uniq(input):
+    s=len(input)
+    output=[]
+    for x in range(s):
+        k=x+1
+        for j in range(k,s):
+            if((input[x] == input[j]) and input[x] not in output):
+                output.append(input[x])
+    for i in output:
+        print(i,end=" ")
+if(len(a)==n):
+    uniq(a)
